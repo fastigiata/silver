942d124c52ec46c06e253a36e1b37f9a5ea1d3ee
lopo <lopo@zju.edu.cn>
2023-12-30 23:09:10 +0800

    feat: StickerPage -- with loader

---
+++ src/pages/Sticker/index.tsx
@@ -0,0 +1,26 @@
+import type { LoaderFunction } from 'react-router-dom'
+
+/**
+ * Generally speaking,
+ * this page should only be displayed when embedded and should not be seen on the web page.
+ * When embedded, it occupies the entire window to simulate a sticky note.
+ */
+const StickerPage = () => {
+    return (
+        <div>
+            <h1>StickerPage</h1>
+        </div>
+    )
+}
+
+/**
+ * Loads the sticker from the database
+ */
+const stickerLoader: LoaderFunction = () => {
+    return null
+}
+
+export {
+    StickerPage,
+    stickerLoader
+}
