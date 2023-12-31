b1a44fbca15499d8e2459b15b4fd1e572cd0dd03
lopo <lopo@zju.edu.cn>
2023-12-31 22:24:01 +0800

    feat: StickerManager

---
+++ src/managers/sticker.ts
@@ -0,0 +1,32 @@
+import type { StickerRecord } from '../declaration'
+import { createStickerWindow, resetToDefaultById } from '../utils/window.ts'
+
+/**
+ * Sticker manager
+ */
+abstract class StickerManager {
+    /**
+     * Show the sticker in separate window
+     *
+     * @returns `false` if the sticker-window is already existed, `true` otherwise
+     */
+    static async showExternal(sticker: StickerRecord) {
+        if (sticker.pin) return false
+
+        createStickerWindow(sticker)
+        return true
+    }
+
+    /**
+     * Reset the sticker-window to default size and position
+     */
+    static async resetToDefault(sticker: StickerRecord) {
+        if (!sticker.pin) return false
+
+        return await resetToDefaultById(sticker.id)
+    }
+}
+
+export {
+    StickerManager
+}
