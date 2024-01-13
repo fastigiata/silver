6c46d574c1a799ec568f60103dd2a46631e8189e
lopo <lopo@zju.edu.cn>
2024-01-13 22:33:59 +0800

    feat: logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,17 +1,68 @@
-class Logger {
-    static #singleton: Logger
+import {
+    attachConsole,
+    debug,
+    info,
+    warn,
+    error,
+} from '@tauri-apps/plugin-log'
+import { Env } from './env.ts'
 
-    constructor() {
-        if (Logger.#singleton) {
-            return Logger.#singleton
-        }
-        Logger.#singleton = this
-    }
+abstract class Logger {
+    abstract initialize(): Promise<void>
 
-    // TODO: proxy all methods on log-plugin
+    abstract verbose(s: string): void
+
+    abstract info(s: string): void
+
+    abstract warn(s: string): void
+
+    abstract error(s: string): void
 }
 
-const logger = new Logger()
+class EmbedLogger extends Logger {
+    public async initialize() {
+        await attachConsole()
+    }
+
+    verbose(s: string) {
+        debug(s)
+    }
+
+    info(s: string) {
+        info(s)
+    }
+
+    warn(s: string) {
+        warn(s)
+    }
+
+    error(s: string) {
+        error(s)
+    }
+}
+
+class WebLogger extends Logger {
+    public async initialize() {
+    }
+
+    verbose(s: string) {
+        console.debug(s)
+    }
+
+    info(s: string) {
+        console.info(s)
+    }
+
+    warn(s: string) {
+        console.warn(s)
+    }
+
+    error(s: string) {
+        console.error(s)
+    }
+}
+
+const logger: Logger = Env.isEmbed ? new EmbedLogger() : new WebLogger()
 
 export {
     logger
