e248727fabbe32a1df26bba5a5f613a50210ec09
lopo <lopo@zju.edu.cn>
2024-01-16 00:02:57 +0800

    test: misc

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,32 @@
+import { DateTimePicker } from '../../components/DateTimePicker.tsx'
+import { useEffect } from 'react'
+import { notifier } from '@/utils/notify.ts'
+
 /**
  * dashboard to manage all stickers
  */
 const DashboardPage = () => {
+    useEffect(() => {
+        window.addEventListener('notificationclick', (ev) => {
+            console.log(ev)
+        })
+    }, [])
+
     return (
         <div className={''}>
+            <p>Since there is no dashboard yet, this is a draft page</p>
+            <br/><br/>
             <h1 className={'text-primary'}>DashboardPage</h1>
             <h1 className={'text-secondary'}>DashboardPage</h1>
             <h1 className={'text-tertiary'}>DashboardPage</h1>
+
+            <button onClick={() => {
+                notifier.notify('xxx')
+            }}>notify</button>
+
+            <DateTimePicker onSelected={d => {
+                console.log(d.toLocaleString())
+            }}/>
         </div>
     )
 }
