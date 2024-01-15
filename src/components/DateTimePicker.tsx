cb64940dc6d9d243428103f9f4e2c3301cc67956
lopo <lopo@zju.edu.cn>
2024-01-15 23:38:32 +0800

    feat: component -- DateTimePicker

--- src/components/DateTimePicker.tsx
+++ src/components/DateTimePicker.tsx
@@ -2,13 +2,14 @@ import ZhCN from 'air-datepicker/locale/zh'
 import { useEffect, useRef } from 'react'
 import AirDatepicker from 'air-datepicker'
 
-const DateTimePicker = ({ onSelected }: { onSelected: (date: Date) => void }) => {
+const DateTimePicker = ({ className, onSelected }: { className?: string, onSelected: (date: Date) => void }) => {
     const ref = useRef<HTMLInputElement>(null)
 
     useEffect(() => {
         const dp = new AirDatepicker(ref.current!, {
             locale: ZhCN,
             autoClose: true,
+            // isMobile: true,
             toggleSelected: false,
             timepicker: true,
             minutesStep: 5,
@@ -20,7 +21,7 @@ const DateTimePicker = ({ onSelected }: { onSelected: (date: Date) => void }) =>
         return () => dp.destroy()
     }, [])
 
-    return <input ref={ref} type="text" readOnly/>
+    return <input ref={ref} className={className} type="text" readOnly/>
 }
 
 export {
