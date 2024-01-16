280b15f378fd058b5c429a4febc3e00027b7e712
lopo <lopo@zju.edu.cn>
2024-01-17 00:16:57 +0800

    test: misc

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,15 @@
-import { DateTimePicker } from '../../components/DateTimePicker.tsx'
+import { DateTimePicker } from '@/components/DateTimePicker.tsx'
 import { useEffect } from 'react'
 import { notifier } from '@/utils/notify.ts'
+
+import { channels, isPermissionGranted, requestPermission, sendNotification, } from '@tauri-apps/plugin-notification'
 
 /**
  * dashboard to manage all stickers
  */
 const DashboardPage = () => {
     useEffect(() => {
-        window.addEventListener('notificationclick', (ev) => {
-            console.log(ev)
-        })
+        // console.log()
     }, [])
 
     return (
@@ -20,9 +20,37 @@ const DashboardPage = () => {
             <h1 className={'text-secondary'}>DashboardPage</h1>
             <h1 className={'text-tertiary'}>DashboardPage</h1>
 
+            <button onClick={async () => {
+                const v = await isPermissionGranted()
+                console.log(`isPermissionGranted: ${v}`)
+            }}>isPermissionGranted
+            </button>
+            <br/>
+
+            <button onClick={async () => {
+                const per = await requestPermission()
+                console.log(per)
+            }}>requestPermission
+            </button>
+            <br/>
+
+            <button onClick={async () => {
+                sendNotification('xxasdasd')
+            }}>sendNotification
+            </button>
+            <br/>
+
+            <button onClick={async () => {
+                const notificationChannels = await channels()
+                console.log(notificationChannels)
+            }}>channels
+            </button>
+            <br/>
+
             <button onClick={() => {
                 notifier.notify('xxx')
-            }}>notify</button>
+            }}>notify
+            </button>
 
             <DateTimePicker onSelected={d => {
                 console.log(d.toLocaleString())
