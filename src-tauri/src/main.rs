ff0185748116a8267675fe0b9b31733dad2eebea
lopo <lopo@zju.edu.cn>
2024-01-18 17:48:08 +0800

    feat(deps): add plugin -- process

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -17,6 +17,7 @@ fn main() {
             Target::new(TargetKind::Webview),
         ]).build())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_window::init())
         .plugin(tauri_plugin_shell::init())
         // .plugin(tauri_plugin_store::Builder::default().build())
