3ea79c455673fb5cb6fc3e8e154f5c945533d3f5
lopo <lopo@zju.edu.cn>
2024-01-21 00:02:17 +0800

    chore: debug log

--- src/utils/app_controller.ts
+++ src/utils/app_controller.ts
@@ -4,6 +4,7 @@ import { exit } from '@tauri-apps/plugin-process'
 abstract class AppController {
     public static async hideToTray() {
         // TODO: Implement
+        console.debug('TODO')
     }
 
     public static async close() {
