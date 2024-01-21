6707363fdd4e8a3ba38ae2e9c8e03b12f5ac2dcd
lopo <lopo@zju.edu.cn>
2024-01-21 23:46:49 +0800

    feat(style): ...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,18 @@ export default <Config>{
         },
         backgroundColor: {
             'transparent': 'transparent',
+            'white': '#FFFFFF',
             'header': 'var(--header-bg)',
             'body': 'var(--body-bg)',
+            'primary-button': 'var(--primary-button-bg)',
         },
         boxShadow: {
             'nav': '0px 2px 8px var(--header-shadow-color)',
+            'card': '1px 1px 4px var(--card-shadow-color)',
+            'card_hover': '3px 3px 4px var(--card-shadow-color)',
         },
         colors: {
+            'white': '#FFFFFF',
             'header': 'var(--header-text-color)',
             'primary': 'var(--primary-text-color)',
             'secondary': 'var(--secondary-text-color)',
@@ -26,12 +31,18 @@ export default <Config>{
         fontSize: {
             'header': 'var(--header-font-size)',
         },
+        fontWeight: {
+            'primary': 'var(--primary-font-weight)',
+            'secondary': 'var(--secondary-font-weight)',
+            'tertiary': 'var(--tertiary-font-weight)',
+        },
         extend: {
             zIndex: {
                 1: '1',
             },
             height: {
                 'header': 'var(--header-height)',
+                'body': 'calc(100% - var(--header-height))',
             },
         },
     },
