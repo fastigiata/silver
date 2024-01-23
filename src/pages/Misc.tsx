c5d1b6dc89dff200f58b734131fc6ea23ed05d89
lopo <lopo@zju.edu.cn>
2024-01-23 23:28:43 +0800

    test: Interactive

--- src/pages/Misc.tsx
+++ src/pages/Misc.tsx
@@ -1,8 +1,27 @@
 import * as Icons from '@/components/Icons.tsx'
+import { Interactive } from '@/components/Interactive.tsx'
+import { useState } from 'react'
 
 const MiscPage = () => {
+    const [ pos, setPos ] = useState({ left: 0, top: 0 })
+
     return (
         <div className={'w-full h-full p-4'}>
+            <Interactive
+                className={'relative h-24 bg-[#ccc]'}
+                onMove={(pos) => {
+                    console.log('pos', pos)
+                    setPos(pos)
+                }}>
+                <div
+                    className="absolute w-4 h-4 bg-[red]"
+                    style={{
+                        left: `${100 * pos.left}%`,
+                        top: `${100 * pos.top}%`,
+                    }}/>
+            </Interactive>
+
+            <div className={'w-full h-[1px] my-8 bg-primary-button'}/>
             <ul className={'space-y-2'}>
                 {
                     Object.entries(Icons)
