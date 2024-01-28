fb1e402cedc73965379e9e3ddc431469a1194735
lopo <lopo@zju.edu.cn>
2024-01-28 23:07:09 +0800

    feat(temp): hideToTray

--- src/utils/app_controller.ts
+++ src/utils/app_controller.ts
@@ -1,10 +1,12 @@
 import { Env } from '@/utils/env.ts'
 import { exit } from '@tauri-apps/plugin-process'
+import { getCurrent } from '@tauri-apps/api/window'
 
 abstract class AppController {
     public static async hideToTray() {
         // TODO: Implement
         console.debug('TODO')
+        return getCurrent().minimize()
     }
 
     public static async close() {
