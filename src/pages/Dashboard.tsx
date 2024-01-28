4ad5d8158ed9230521e81dcb984d0d34b075b945
lopo <lopo@zju.edu.cn>
2024-01-28 23:24:44 +0800

    chore: mics

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -15,21 +15,29 @@ const DashboardPage = () => {
         <div className={''}>
             <p>Since there is no dashboard yet, this is a draft page</p>
             <br/><br/>
-            <h1 className={'text-primary'}>DashboardPage</h1>
-            <h1 className={'text-secondary'}>DashboardPage</h1>
-            <h1 className={'text-tertiary'}>DashboardPage</h1>
+            <h1 className={'text-primary'}>text-primary: DashboardPage</h1>
+            <h1 className={'text-secondary'}>text-secondary: DashboardPage</h1>
+            <h1 className={'text-tertiary'}>text-tertiary: DashboardPage</h1>
 
-            <Link className={'underline underline-offset-4'} to={'/misc'}>Misc</Link>
+            <br/><br/>
+
+            <Link className={'underline underline-offset-4'} to={'/misc'}>to page /misc</Link>
+
+            <br/><br/>
 
             <button onClick={() => {
                 notifier.notify('xxx')
-            }}>notify
+            }}>notify 1
             </button>
+            <br/>
             <button onClick={() => {
                 notifier.notify('标题5个字', '已过期')
-            }}>notify
+            }}>notify 2
             </button>
 
+            <br/><br/>
+
+            <p>DatePicker</p>
             <DateTimePicker onSelected={d => {
                 console.log(d.toLocaleString())
             }}/>
--- src/pages/Misc.tsx
+++ src/pages/Misc.tsx
@@ -2,6 +2,7 @@ import * as Icons from '@/components/Icons.tsx'
 import { Interactive } from '@/components/Interactive.tsx'
 import { useState } from 'react'
 import { MacScrollbar } from 'mac-scrollbar'
+import { AwesomeScrollbar } from '@/components/AwesomeScrollbar.tsx'
 
 const str = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
@@ -9,7 +10,8 @@ const MiscPage = () => {
     const [ pos, setPos ] = useState({ left: 0, top: 0 })
 
     return (
-        <div className={'w-full h-full p-4'}>
+        <AwesomeScrollbar className={'w-full h-full p-4'}>
+            <p>Click and/or Drag</p>
             <Interactive
                 className={'relative h-24 bg-[#ccc]'}
                 onMove={(pos) => {
@@ -17,7 +19,7 @@ const MiscPage = () => {
                     setPos(pos)
                 }}>
                 <div
-                    className="absolute w-4 h-4 bg-[red]"
+                    className="absolute w-4 h-4 bg-[green]"
                     style={{
                         left: `${100 * pos.left}%`,
                         top: `${100 * pos.top}%`,
@@ -46,7 +48,7 @@ const MiscPage = () => {
                         </li>)
                 }
             </ul>
-        </div>
+        </AwesomeScrollbar>
     )
 }
 
