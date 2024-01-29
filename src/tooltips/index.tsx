a087dc0b3a5a195d754aa6f2337d5143daf7fc47
lopo <lopo@zju.edu.cn>
2024-01-29 23:55:33 +0800

    feat: ValuePicker

--- src/tooltips/index.tsx
+++ src/tooltips/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef, useState } from 'react'
 import type { TooltipRefProps } from 'react-tooltip'
-import { ControlledTooltip } from '@/tooltips/ControlledTooltip.tsx'
+import { ControlledTooltip } from '@/tooltips/shared.tsx'
 import { HexAlphaColorPicker, HexColorPicker } from 'react-colorful'
-import { useColorPicker } from '@/tooltips/ColorPicker.tsx'
+import { useColorPicker } from '@/tooltips/hooks/ColorPicker'
 import { IconCheck, IconCross } from '@/components/Icons.tsx'
+import { useValuePicker } from '@/tooltips/hooks/ValuePicker.tsx'
+import { Interactive } from '@/components/Interactive.tsx'
 
 const ColorPicker = () => {
     const ref = useRef<TooltipRefProps>(null)
@@ -48,12 +50,62 @@ const ColorPicker = () => {
     )
 }
 
+const ValuePicker = () => {
+    const ref = useRef<TooltipRefProps>(null)
+    const { isOpen, open, close, details } = useValuePicker()
+
+    const [ defaultValue, min, max, step = 1 ] = details.config
+    const [ v, _setV ] = useState({ rate: (defaultValue - min) / (max - min), mapped: defaultValue })
+    const setV = (to: number) => {
+        const range = max - min
+        const mapped = Math.round(to * range / step) * step + min
+        _setV({ rate: (mapped - min) / range, mapped })
+    }
+
+    useEffect(() => {
+        // update value when config changed
+        const [ defaultValue, min, max ] = details.config
+        const rate = (defaultValue - min) / (max - min)
+        _setV({ rate, mapped: defaultValue })
+    }, [ details.config ])
+
+    return (
+        <ControlledTooltip
+            id={'value-picker'} ref={ref}
+            anchorId={details.anchorId}
+            isOpen={isOpen}
+            setIsOpen={(to) => {
+                if (to) {
+                    open(details.anchorId!, details.config, details.closeCallback)
+                } else {
+                    close(null)
+                }
+            }}>
+            <div className={'w-[200px] h-[16px] text-[12px] flex items-center justify-between'}>
+                <span>{min}</span>
+                <Interactive
+                    className={'relative flex-1 h-full mx-1 rounded-[4px] bg-[#ccc]'}
+                    onMove={(pos) => setV(pos.left)}>
+                    <div className={
+                        'absolute top-0 left-0 h-full rounded-l-[4px] bg-[green] ' +
+                        'leading-[16px] text-center'
+                    } style={{ width: `${100 * v.rate}%` }}>
+                        {v.mapped}
+                    </div>
+                </Interactive>
+                <span>{max}</span>
+            </div>
+        </ControlledTooltip>
+    )
+}
+
 /**
  * Contains all the tooltips used in the app.
  */
 const Tooltips = () => {
     return <>
         <ColorPicker/>
+        <ValuePicker/>
     </>
 }
 
