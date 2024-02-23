eba94f6f02a3c2d629a246cc67bc77446304dcad
lopo <lopo@zju.edu.cn>
2024-02-24 00:00:17 +0800

    feat(todo): handleCreate

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import { Loading } from '@/components/Loading.tsx'
 import { CollectionDB } from '@/db/collection.ts'
 import type { ICollection } from '@/_types/collection.ts'
 import { CollectionCard } from '@/components/Card/CollectionCard.tsx'
+import { AwesomeScrollbar } from '@/components/AwesomeScrollbar.tsx'
 
 type Loader = {
     collection: Promise<ICollection[]>
@@ -13,7 +14,7 @@ const CollectionList = () => {
     const collections = useAsyncValue() as ICollection[]
 
     return collections.map(collection => {
-        return <CollectionCard key={collection.id} {...collection}/>
+        return <CollectionCard key={collection.id} className={'my-4'} {...collection}/>
     })
 }
 
@@ -23,14 +24,26 @@ const CollectionList = () => {
 const DashboardPage = () => {
     const loader = useLoaderData() as Loader
 
+    const handleCreate = () => {
+        // TODO: create new collection
+        console.log('TODO')
+    }
+
     return (
-        <div className={'w-full h-full p-4'}>
+        <AwesomeScrollbar className={'w-full h-full p-4 overflow-y-auto'}>
             <Suspense fallback={<Loading/>}>
+                <div className={'w-full h-6 flex items-center'}>
+                    <button className={
+                        'as-button text-secondary text-[14px] font-secondary underline underline-offset-4'
+                    } onClick={handleCreate}>
+                        New Collection
+                    </button>
+                </div>
                 <Await resolve={loader.collection}>
                     <CollectionList/>
                 </Await>
             </Suspense>
-        </div>
+        </AwesomeScrollbar>
     )
 }
 
