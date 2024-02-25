b8916bd766c2e7b09af06126d200eab74d179728
lopo <lopo@zju.edu.cn>
2024-02-25 22:58:28 +0800

    feat: add action buttons

--- src/components/Card/CollectionCard.tsx
+++ src/components/Card/CollectionCard.tsx
@@ -1,6 +1,30 @@
 import type { ICollection } from '@/_types/collection.ts'
+import type { ExtendedIcon } from '@/components/Icons.tsx'
+import { IconDelete } from '@/components/Icons.tsx'
 
-const CollectionCard = ({ id, ctime, mtime, name, desc, className = '' }: ICollection & { className?: string }) => {
+const ActionButton = ({ className = '', Icon, onClick }: {
+    className?: string,
+    Icon: ExtendedIcon,
+    onClick?: VoidFunction
+}) => {
+    return (
+        <div className={
+            `${className} ` +
+            'as-button w-8 h-8 rounded-full bg-white ' +
+            'shadow-card hover:shadow-card_hover ' +
+            'text-[18px] flex items-center justify-center'
+        } onClick={onClick}>
+            <Icon/>
+        </div>
+    )
+}
+
+const CollectionCard = ({ collection, className = '', onDelete }: {
+    collection: ICollection,
+    className?: string,
+    onDelete?: VoidFunction
+}) => {
+    const { id, name, desc, ctime, mtime } = collection
     return (
         <div
             data-id={id}
@@ -25,8 +49,8 @@ const CollectionCard = ({ id, ctime, mtime, name, desc, className = '' }: IColle
                 <span>Modified: {new Date(mtime).toLocaleString()}</span>
             </div>
 
-            <div className={'absolute z-1 right-4 hidden group-hover:flex items-center'}>
-                TODO: actions
+            <div className={'absolute z-1 right-4 hidden group-hover:flex items-center space-x-2'}>
+                <ActionButton className={'text-red'} Icon={IconDelete} onClick={onDelete}/>
             </div>
         </div>
     )
