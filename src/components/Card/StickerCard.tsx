7b0e7a7168adcaa82d21fed09692399403fcf549
lopo <lopo@zju.edu.cn>
2024-03-06 23:18:13 +0800

    feat: StickerCard -- partial

--- src/components/Card/StickerCard.tsx
+++ src/components/Card/StickerCard.tsx
@@ -9,8 +9,19 @@ type StickerCardProps = {
 
 const StickerCard = ({ sticker, onClick, onModify, onDelete }: StickerCardProps) => {
     return (
-        <div>
-            xx
+        <div className={'relative w-44 h-56 bg-white shadow-card hover:shadow-card_hover'}>
+            <div className={'w-full h-8 px-2 bg-[#F7F8FA] text-primary font-primary leading-[32px] line-clamp-1'}>
+                {sticker.title}
+            </div>
+
+            <div className={
+                'w-full h-[calc(100%-48px)] my-2 px-2 ' +
+                'text-secondary text-[14px] font-secondary leading-[22px] text-justify line-clamp-[8]'
+            }>
+                {sticker.content}
+            </div>
+
+            {/* TODO: hover时弹出操作按钮 */}
         </div>
     )
 }
