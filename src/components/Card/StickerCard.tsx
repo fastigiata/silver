9395fedffebc8f1a899e4914b67219f1c3924017
lopo <lopo@zju.edu.cn>
2024-03-08 23:39:33 +0800

    feat: StickerCard

--- src/components/Card/StickerCard.tsx
+++ src/components/Card/StickerCard.tsx
@@ -1,18 +1,35 @@
 import type { ISticker } from '@/_types/sticker.ts'
+import { IconBell, IconDelete, IconEdit } from '@/components/Icons.tsx'
+import dayjs from 'dayjs'
 
 type StickerCardProps = {
     sticker: ISticker,
-    onClick?: VoidFunction
     onModify?: VoidFunction
     onDelete?: VoidFunction
 }
 
-const StickerCard = ({ sticker, onClick, onModify, onDelete }: StickerCardProps) => {
+const AlarmBanner = ({ alarm }: { alarm?: number | null }) => {
+    if (!alarm) return null
+
+    const done = alarm < Date.now()
+    const text = dayjs(alarm).format('YYYY-MM-DD HH:mm')
+
+    return (
+        <div className={'w-full h-8 px-2 flex items-center'}>
+            <IconBell className={'mr-1 text-tertiary text-[14px]'}/>
+            <span className={`text-tertiary text-[12px] font-tertiary ${done ? 'line-through' : ''}`}>
+                {text}
+            </span>
+        </div>
+    )
+}
+
+const StickerCard = ({ sticker, onModify, onDelete }: StickerCardProps) => {
     const themeId = sticker.theme ?? 0
 
     return (
         <div className={
-            'group relative w-44 h-56 rounded-[4px] bg-white shadow-card hover:shadow-card_hover overflow-hidden '
+            'group relative w-44 h-56 rounded-[4px] bg-white shadow-card hover:shadow-card_hover overflow-hidden flex flex-col'
         }>
             <div className={
                 `bg-ps${themeId} ` +
@@ -22,19 +39,24 @@ const StickerCard = ({ sticker, onClick, onModify, onDelete }: StickerCardProps)
             </div>
 
             <div className={
-                'w-full h-[calc(100%-48px)] my-2 px-2 ' +
+                // `bg-ps${themeId}-light ` +
+                'w-full flex-1 my-2 px-2 ' +
                 'text-secondary text-[14px] font-secondary leading-[22px] text-justify line-clamp-[8]'
             }>
                 {sticker.content}
             </div>
 
-            {/* TODO: hover时弹出操作按钮 */}
-            <div
-                className={
-                    `bg-ps${themeId}-light ` +
-                    'absolute z-1 w-full -bottom-full group-hover:bottom-0 h-8 border-t-2 transition-[bottom]'
-                }>
-                hello
+            <AlarmBanner alarm={sticker.alarm}/>
+
+            <div className={
+                'absolute z-1 w-full h-8 -bottom-full group-hover:bottom-0 border-t-2 bg-white transition-[bottom]'
+            }>
+                <div className={
+                    `bg-ps${themeId}-light text-ps${themeId} ` +
+                    'w-full h-full px-2 flex items-center space-x-1'}>
+                    <IconEdit className={'as-button w-6 h-6'} onClick={onModify}/>
+                    <IconDelete className={'as-button w-6 h-6'} onClick={onDelete}/>
+                </div>
             </div>
         </div>
     )
