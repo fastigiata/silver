be8406435abf0d5668b1711ed90336bbf48db1c4
lopo <lopo@zju.edu.cn>
2024-03-14 23:30:50 +0800

    test: notify impl -- pass

--- src/pages/Misc.tsx
+++ src/pages/Misc.tsx
@@ -1,9 +1,26 @@
 import * as Icons from '@/components/Icons.tsx'
 import { AwesomeScrollbar } from '@/components/AwesomeScrollbar.tsx'
+import { NotifyImpl } from '@/utils/notify.ts'
 
 const MiscPage = () => {
+    const sendNotify = () => {
+        NotifyImpl.notify({
+            title: `title ${new Date().toLocaleString()}`,
+            data: {
+                name: 'name',
+                age: 18
+            },
+            onClick: (v) => {
+                console.log('onClick', v)
+                window.open('https://www.baidu.com', '_blank')
+            }
+        })
+    }
+
     return (
         <AwesomeScrollbar className={'w-full h-full p-4'}>
+            <button onClick={sendNotify}>send notify</button>
+            <div className={'w-full h-[2px] my-2 bg-[#CCC]'}></div>
             <p>图标</p>
             <ul className={'space-y-2'}>
                 {
