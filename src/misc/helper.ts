1a9c92a6054b0d26e201c17af3787663b747e92d
lopo <lopo@zju.edu.cn>
2024-03-24 23:02:42 +0800

    fix: non-https fallback

--- src/misc/helper.ts
+++ src/misc/helper.ts
@@ -8,7 +8,23 @@ const formatToN = (str: string, n: number) => {
     return str.length <= n ? str : `${str.slice(0, n)}...`
 }
 
+const _fallback = (): Promise<File | null> => {
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.accept = '.json,.yaml,.toml'
+    const promise = new Promise<File | null>((resolve) => {
+        input.onchange = () => {
+            const file = input.files?.[0]
+            resolve(file || null)
+        }
+    })
+    input.click()
+    return promise
+}
+
 const getArchiveFile = async (): Promise<File | null> => {
+    if (!('showOpenFilePicker' in window)) return _fallback()
+
     try {
         // @ts-expect-error ...
         const handler: FileSystemFileHandle[] = await window.showOpenFilePicker({
