457c32c0c6c4951b9d51b7f3e0ae52b8b3669e68
lopo <lopo@zju.edu.cn>
2024-03-24 22:46:49 +0800

    todo: ...

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,7 +1,16 @@
 const SettingPage = () => {
     return (
-        <div>
-            <h1>Setting Page</h1>
+        <div className={'w-full h-full p-4'}>
+            <h1>
+                TODO: Setting
+            </h1>
+
+            {/* <button onClick={async () => { */}
+            {/*     const r = await StickerDB.check() */}
+            {/*     console.log(r) */}
+            {/*     console.log(StickerDB.check()) */}
+            {/* }}>xxx xxx */}
+            {/* </button> */}
         </div>
     )
 }
