46246daf61815196b0f4191c0c19e0a97836c924
lopo <lopo@zju.edu.cn>
2024-03-25 23:57:31 +0800

    todo: FIXME

--- src/_types/route.ts
+++ src/_types/route.ts
@@ -1,3 +1,7 @@
+import type { To, UIMatch } from 'react-router-dom'
+
 export type RouteHandle = {
+    // FIXME: deprecated
     showBack?: boolean
+    backBuilder?: (match: UIMatch, query: URLSearchParams) => To
 }
